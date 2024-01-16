33ec1e82d474e251a4283fbae6a0449e6cabe5a1
John-thabuks <thabuksjohn@gmail.com>
2024-01-16 14:05:25 +0300

    Assignment completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    my_string = None
+    if len(items) <= 2:
+        my_string = ' and '.join(items) 
+    elif len(items) == 3:        
+        my_string = ', '.join(items[0:2]) + f', and {items[2]}'
+    else:
+        my_string = ', '.join(items[:-1]) +  f", and {items[-1]}"  
+    return my_string
